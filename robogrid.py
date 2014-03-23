8b19c95e2f8291564410cbad2473e637300c6130
Jack Wearden <jack@jackwearden.co.uk>
2014-03-23 21:08:04 +0000

    forward motion

--- robogrid.py
+++ robogrid.py
@@ -1,9 +1,18 @@
 class Robot(object):
     def __init__(self, name):
         self._heading = 0
+        self.x = 0
+        self.y = 0
 
     def forward(self):
-        pass
+        if self.heading == 0:
+            self.y -= 1
+        elif self.heading == 1:
+            self.x += 1
+        elif self.heading == 2:
+            self.y += 1
+        elif self.heading == 3:
+            self.x -= 1
 
     def right(self):
         self.heading += 1
